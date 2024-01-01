5d9b239dadb699cf2cf26af341d0816c5c2b6d2a
soramon0 <23435312+soramon0@users.noreply.github.com>
2024-01-01 03:05:54 +0100

    feat: add command line flags

--- pkg/commands/command.go
+++ pkg/commands/command.go
@@ -1,40 +1,84 @@
 package commands
 
 import (
+	"flag"
 	"fmt"
 
 	termfreq "github.com/soramon0/kirlia/pkg/term_freq"
 )
 
+var (
+	indexCmd = flag.NewFlagSet("index", flag.ExitOnError)
+	serveCmd = flag.NewFlagSet("serve", flag.ExitOnError)
+	commands = map[string]*flag.FlagSet{
+		indexCmd.Name(): indexCmd,
+		serveCmd.Name(): serveCmd,
+	}
+)
+
+type Cmd struct {
+	*flag.FlagSet
+}
+
 func Run(args []string) error {
-	if len(args) < 1 {
-		usage()
-		return fmt.Errorf("error: command is required")
-	}
-
-	if args[0] != "index" {
-		usage()
-		return fmt.Errorf("error: invalid command")
-	}
-
-	if len(args) < 2 || args[1] == "" {
-		usage()
-		return fmt.Errorf("error: file name is required")
-	}
-
-	targetPath := args[1]
-	tfIndex, err := termfreq.NewIndex(targetPath)
+	cmd, err := parseCommand(args)
 	if err != nil {
+		usage()
 		return err
 	}
 
-	fmt.Printf("Indexed %d files in %s ...\n", len(tfIndex), targetPath)
+	return cmd.Execute(args[1:])
+}
+
+func (c *Cmd) Execute(args []string) error {
+	if c == nil {
+		return fmt.Errorf("error: uknown command")
+	}
+
+	cmdName := c.Name()
+	switch cmdName {
+	case indexCmd.Name():
+		targetPath := c.String("f", "", "directory or file to index")
+		if err := c.Parse(args); err != nil {
+			return err
+		}
+		if *targetPath == "" {
+			return fmt.Errorf("error: file name is required")
+		}
+
+		tfIndex, err := termfreq.NewIndex(*targetPath)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("Indexed %d files in %s ...\n", len(tfIndex), *targetPath)
+
+	case serveCmd.Name():
+		return fmt.Errorf("error: serve command not implemented yet")
+
+	default:
+		return fmt.Errorf("error: unknown %q command", cmdName)
+	}
+
 	return nil
+}
+
+func parseCommand(args []string) (*Cmd, error) {
+	if len(args) < 1 {
+		return nil, fmt.Errorf("error: a command is required")
+	}
+
+	cmd := commands[args[0]]
+	if cmd == nil {
+		return nil, fmt.Errorf("error: uknown %q command", args[0])
+	}
+
+	return &Cmd{cmd}, nil
 }
 
 func usage() {
 	fmt.Println("usage:")
-	fmt.Println("  kirlia [command] (options)")
+	fmt.Println("  kirlia command [-options]")
 	fmt.Println("    - available commands:")
-	fmt.Println("      - index filename")
+	fmt.Println("      - index")
+	fmt.Println("      - serve")
 }
